4a6b4ad5a2d539039ed811b41c49ed0e46f4f219
Russell Osborne <russell@burningpony.com>
2013-03-02 14:50:51 -0500

    Adding Custom Link Support via Keyed Hash in config files

--- config_demo.php
+++ config_demo.php
@@ -1,13 +1,20 @@
 <?php
 // necessary to work
 $mysql_host = 'localhost'; // or 127.0.0.1
-$mysql_user = 'MYSQL_USER'; 
+$mysql_user = 'MYSQL_USER';
 $mysql_pass = 'MYSQL_PASSWORD';
 $mysql_db = 'MYSQL_DATABASE_NAME';
-$mysql_encoding = 'latin1';  // this is recommended don't know why but it is. 
+$mysql_encoding = 'latin1';  // this is recommended don't know why but it is.
 $prefix = 'Stats_'; // [default=stats]
 
 // not necessary, but cool
 $server_ip = 'example.com'; // can be an real ip or an dns alias
 $server_port = 25565; // [default=25565]
+
+
+$custom_links = [
+  "Dynamap" => $server_ip . ":8123",
+  "Mojang" => "http://mojang.com"
+];
+
 ?>
