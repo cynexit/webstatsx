af9ccf1225c1613b1f3cb96de51ae3766564f04a
cynexit <cynexit@cynexit.com>
2013-03-06 11:33:27 +0100

    Some more explanations

--- config_demo.php
+++ config_demo.php
@@ -7,16 +7,27 @@ $mysql_db = 'MYSQL_DATABASE_NAME';
 $mysql_encoding = 'latin1';  // this is recommended because the plugin creates all its tables with latin1 encoding
 $prefix = 'Stats_'; // [default=stats]
 
-// not necessary, but cool
+
+
+/*########################
+ optional stuff
+########################*/
+
+//--- online/offline check
 $server_ip = 'example.com'; // can be an real ip or an dns alias
 $server_port = 25565; // [default=25565]
 
-$link_to_map = "http://".$server_ip . ":8123"; // Don't want the link to be displayed? Use: $link_to_map = "";
+//--- add a link to the online map
+$link_to_map = "http://".$server_ip . ":8123";
+// Don't want the link to be displayed? Uncomment the next line!
+//$link_to_map = ""
 
+//--- add custom links to the menu
 $custom_links = array(
   "Dynamap" => $link_to_map,
   "Mojang" => "http://mojang.com"
 );
 
+//--- enable the server stats page [beta]
 $enable_server_page = false; //BETA!! Needs some special rights your webserver might not have on the host system
 ?>
