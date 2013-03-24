21bd8b755d2cd676f90749bca78a78ee875da85b
cynexit <cynexit@cynexit.com>
2013-03-24 13:08:26 +0100

    start rm js/

--- footer.php
+++ footer.php
@@ -45,8 +45,6 @@
 	<script src="js/jquery.flot.pie.min.js"></script>
 	<script src="js/jquery.flot.stack.js"></script>
 	<script src="js/jquery.flot.resize.min.js"></script>
-	<script src="js/bootstrap-popover.js"></script>
-
 
 	<script src="js/charisma.js"></script>	
 </body>
